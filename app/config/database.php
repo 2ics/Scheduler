2a43a675515ba6c867d9c8cba9bd446bf10b6e73
Sean Buller <seanbuller@hotmail.com>
2014-07-30 21:00:16 -0400

    Configured Database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'l4sentry',
-			'username'  => 'dbuser',
-			'password'  => 'dbpassword',
+			'database'  => 'timemanagement',
+			'username'  => 'root',
+			'password'  => 'admin',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
