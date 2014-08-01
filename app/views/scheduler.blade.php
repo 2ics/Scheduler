ffe3f1b3f29e0d9eb8e655281708212a063ea35d
Sean Buller <seanbuller@hotmail.com>
2014-08-01 11:54:38 -0400

    Working on scheduler

--- app/views/scheduler.blade.php
+++ app/views/scheduler.blade.php
@@ -10,95 +10,41 @@
 @section('content')
 <div class="row">
   <div class="col-md-3">
-<div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class=""><span class="glyphicon glyphicon-folder-close">
-                    </span>Content</a>
-                </h4>
-              </div>
-              <div id="collapseOne" class="panel-collapse collapse in">
-                <ul class="list-group">
-                  <li class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span><a href="http://fb.com/moinakbarali">Articles</a>
-                    <ul class="list-group">
-                      <li class="list-group-item"><span class="glyphicon glyphicon-pencil text-primary"></span><a href="http://fb.com/moinakbarali">Articles</a></li>
-
-                      <li class="list-group-item"><span class="glyphicon glyphicon-flash text-success"></span><a href="http://fb.com/moinakbarali">News</a></li>
-
-                      <li class="list-group-item"><span class="glyphicon glyphicon-file text-info"></span><a href="http://fb.com/moinakbarali">Newsletters</a></li>
-
-                      <li class="list-group-item"> <span class="glyphicon glyphicon-comment text-success"></span><a href="http://fb.com/moinakbarali">Comments</a><span class="badge">42</span></li>
-
-                    </ul>
-                  </li>
-
-                  <li class="list-group-item"><span class="glyphicon glyphicon-flash text-success"></span><a href="http://fb.com/moinakbarali">News</a></li>
-
-                  <li class="list-group-item"><span class="glyphicon glyphicon-file text-info"></span><a href="http://fb.com/moinakbarali">Newsletters</a></li>
-
-                  <li class="list-group-item"> <span class="glyphicon glyphicon-comment text-success"></span><a href="http://fb.com/moinakbarali">Comments</a><span class="badge">42</span></li>
-
-                </ul>
-              </div>
-            </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" class="collapsed"><span class="glyphicon glyphicon-file">
-                    </span>Reports</a>
-                </h4>
-              </div>
-              <div id="collapseFour" class="panel-collapse collapse">
-                <div class="list-group">
-                  <a href="#" class="list-group-item">
-                    Cras justo odio
-                  </a>
-                  <div class="list-group">
-                    <a href="#" class="list-group-item">
-                      Cras justo odio
-                    </a>
-                    <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-                    <a href="#" class="list-group-item">Morbi leo risus</a>
-                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-                    <a href="#" class="list-group-item">Vestibulum at eros</a>
-                  </div>
-                  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-                  <a href="#" class="list-group-item">Morbi leo risus</a>
-                  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-                  <a href="#" class="list-group-item">Vestibulum at eros</a>
-                </div>
-              </div>
-            </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive" class="collapsed"><span class="glyphicon glyphicon-heart">
-                    </span>Reports</a>
-                </h4>
-              </div>
-              <div id="collapseFive" class="panel-collapse collapse">
-                <div class="list-group">
-                  <a href="#" class="list-group-item">
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                  </a>
-                </div>
-                <div class="list-group">
-                  <a href="#" class="list-group-item active">
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                  </a>
-                </div>
-                <div class="list-group">
-                  <a href="#" class="list-group-item">
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                  </a>
-                </div>
-              </div>
-            </div>
+	<div class="panel-group">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a>TASKS</a>
+            </h4>
           </div>
+          <div class="panel-collapse">
+            <ul class="list-group">
+              <li class="list-group-item"><a data-toggle="collapse" href="#collapseEligible">Eligible Tasks</a><span class="badge">4</span>
+                <ul class="list-group collapse" id="collapseEligible">
+                  <li class="list-group-item">Business Cards<span class="glyphicon glyphicon-search pull-right"></span></li>
+
+                  <li class="list-group-item">Postcards</li>
+
+                  <li class="list-group-item">Newsletters</li>
+
+                  <li class="list-group-item">Annual Reports</li>
+                </ul>
+              </li>
+              <li class="list-group-item"><a data-toggle="collapse" href="#collapseUnplaced" class="">Unplaced Tasks</a><span class="badge">4</span>
+                <ul class="list-group collapse" id="collapseUnplaced">
+                  <li class="list-group-item">Business Cards</li>
+
+                  <li class="list-group-item">Postcards</li>
+
+                  <li class="list-group-item">Newsletters</li>
+
+                  <li class="list-group-item">Annual Reports</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
   </div>
   <div class="col-md-9">
   </div>
