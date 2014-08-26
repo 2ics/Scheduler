9b39453c7a8642a384d8b878bbada8b3df033ff8
SeanBuller <seanbuller@hotmail.com>
2014-08-26 15:48:31 -0400

    Update Config

--- app/config/app.php
+++ app/config/app.php
@@ -1,4 +1,11 @@
 <?php
+if (isset($_SERVER['HTTP_HOST'])){
+	if ($_SERVER['HTTP_HOST'] == "localhost" || $_SERVER['HTTP_HOST'] == "localhost:8888"){
+		$_SERVER['HTTP_HOST'] = "".$_SERVER['HTTP_HOST']."/Scheduler/public";
+	}else if(!strstr($_SERVER['HTTP_HOST'], "http://")){
+		$_SERVER['HTTP_HOST'] = "http://".$_SERVER['HTTP_HOST'];
+	}
+}
 return array(
 
 	/*
--- app/config/packages/stolz/assets/config.php
+++ app/config/packages/stolz/assets/config.php
@@ -1,12 +1,5 @@
 <?php
 if (isset($_SERVER['HTTP_HOST'])){
-	if ($_SERVER['HTTP_HOST'] == "localhost" || $_SERVER['HTTP_HOST'] == "localhost:8888"){
-		$_SERVER['HTTP_HOST'] = "".$_SERVER['HTTP_HOST']."/Scheduler/public";
-	}else{
-		$_SERVER['HTTP_HOST'] = "www.".$_SERVER['HTTP_HOST'];
-	}
-}
-if (isset($_SERVER['HTTP_HOST'])){
 	$url = $_SERVER['HTTP_HOST'];
 }else{
 	$url = "http://localhost:8888/Scheduler/public";
